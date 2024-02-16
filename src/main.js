6e8ebe08db5ff917cd379a656f5cb111c9453412
Yasir-coding <150331916+Yasir-coding@users.noreply.github.com>
2024-02-16 11:21:57 +0100

    anime

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import imageUrl from "./media/baboon.jpg"
+import anime from "animejs";
 
 const img = document.createElement("img");
 document.body.append(img);
 
 img.src = imageUrl;
+
+anime({
+  targets: img,
+  translateX: "300px",
+  duration: 10000,
+  loop: true
+})
